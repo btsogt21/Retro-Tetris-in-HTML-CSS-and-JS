f45090d88d11a3ca072a1d121af331a5a6dfb717
btsogt21 <manlaitsoogii@gmail.com>
2022-06-28 07:46:35 +0800

    Added functionality for moving tetrominoes, tetrominoes that stop when they hit the bottom of the box or another tetromino

--- tetrisapp.js
+++ tetrisapp.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     var htmlElements = "";
-        for (var i = 0; i<200;i++){
-            htmlElements+='<div class = "grid"></div>';
+        for (let i = 0; i<210;i++){
+            if(i<200){
+                htmlElements+='<div class = "grid"></div>';
+            } else{
+                htmlElements+='<div class = "taken"></div>';
+            }
         }
         var container = document.getElementById("container");
         container.innerHTML = htmlElements;
+    
     const grid = document.querySelector('.grid')
     let cells = document.querySelectorAll('.grid div')
-    console.log(cells)
     const ScoreDisplay = document.querySelector('#score')
     const Startbtn = document.querySelector('#startbutton')
     const width = 10
@@ -38,7 +42,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         [width*2, width+1, width*2+1, width+2]
     ]
     const zTetromino = [
-        [width*2, width, width+1, 2],
+        [width*2, width, width+1, 1],
         [width, width+1, width*2+1, width*2+2],
         [width*2, width, width+1, 2],
         [width, width+1, width*2+1, width*2+2]
@@ -56,7 +60,44 @@ document.addEventListener('DOMContentLoaded', ()=>{
         [width, width+1, width+2, width+3]
     ]
     const Tetrominoes = [lTetromino, jTetromino, iTetromino, oTetromino, zTetromino, sTetromino, tTetromino]
+
+
+
+    let random = Math.floor(Math.random()*Tetrominoes.length)
+    let currentPosition = 4
+    let currentRotation = 0
+    let currentTetromino = Tetrominoes[random][currentRotation]
+
+    function draw(){
+        currentTetromino.forEach(index => {
+            cells[currentPosition + index].classList.add('tetromino')
+        });
+    }
+    function undraw(){
+        currentTetromino.forEach(index => {
+            cells[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+    timerId = setInterval(moveDown, 200)
+
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        stop()
+
+    }
+    function stop(){
+        if(currentTetromino.some(index => cells[currentPosition + index + width].classList.contains('taken'))){
+            currentTetromino.forEach(index => cells[currentPosition + index].classList.add('taken'))
+            random = Math.floor(Math.random()*Tetrominoes.length)
+            currentTetromino = Tetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
 })
+
 
 // function populateDivs(){
 //     var htmlElements = "";
