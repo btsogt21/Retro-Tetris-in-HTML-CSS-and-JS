4ad015d560ad68c40a4aa92f7f1612c009f0bac4
btsogt21 <manlaitsoogii@gmail.com>
2022-06-27 21:49:59 +0800

    added rest of the tetrominoes and color styling for them

--- tetrisapp.js
+++ tetrisapp.js
@@ -19,6 +19,43 @@ document.addEventListener('DOMContentLoaded', ()=>{
         [width*2, width*2+1, width+1, 1],
         [width, width*2, width*2+1, width*2+2]
     ]
+    const jTetromino = [
+        [0, 1, width+1, width*2+1, ],
+        [width*2, width, width+1, width+2],
+        [1, width+1, width*2+1, width*2+2],
+        [width*2, width*2+1, width*2+2, width+2]
+    ]
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+    const sTetromino = [
+        [0, width, width+1, width*2+1],
+        [width*2, width+1, width*2+1, width+2],
+        [0, width, width+1, width*2+1],
+        [width*2, width+1, width*2+1, width+2]
+    ]
+    const zTetromino = [
+        [width*2, width, width+1, 2],
+        [width, width+1, width*2+1, width*2+2],
+        [width*2, width, width+1, 2],
+        [width, width+1, width*2+1, width*2+2]
+    ]
+    const tTetromino = [
+        [width, width+2, width+1, width*2+1],
+        [width, width+1, 1, width*2+1],
+        [width*2, width*2+1, width*2+2, width+1],
+        [1, width+1, width*2+1, width+2]
+    ] 
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+    const Tetrominoes = [lTetromino, jTetromino, iTetromino, oTetromino, zTetromino, sTetromino, tTetromino]
 })
 
 // function populateDivs(){
